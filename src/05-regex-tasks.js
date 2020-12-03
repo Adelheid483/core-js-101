3134c85f505aafb89d4a14b8412a9a01e5c552f2
Adelheid483 <adelheid0106@gmail.com>
2020-12-03 09:39:54 +0300

    feat: add some solutions

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,8 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  const regExp = /^\{[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}\}$/;
+  return regExp;
 }
 
 
@@ -54,7 +55,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /.*p.t.*/;
 }
 
 
@@ -78,8 +79,8 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  return RegExp(`^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[\\da-zA-Z]{${minLength},}$`);
 }
 
 
